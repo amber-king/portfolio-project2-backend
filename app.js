18198b8f4e9d709c9f4d46e7864747cfc7bd6539
amber-king <amberking@pursuit.org>
2023-05-23 14:30:40 -0400

    added clarifying/helper notes to needed files

--- app.js
+++ app.js
@@ -6,17 +6,23 @@ const rocksControllers = require("./controllers/rocksControllers");
 // Configuration
 const app = express();
 
-// Miiddleware
+// https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/routes
+// - allows us to group the route handlers for a particular part of a site together and access them using a common route-prefix.
+
+//Miiddleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
+// Routes - for home page
 app.get("/", (req, res) => {
   res.json("Welcome to Rock-World 🗿");
 });
 
-app.use("/rocks", rocksControllers);
+// https://www.tutorialspoint.com/express-js-app-use-method#:~:text=The%20app.,path%20matches%20the%20defined%20path.
+// mounts or puts the specified middleware finctions at the specified path.
 
+// here rockControllers is being mounted here along with rock index page to keep track of change
+app.use("/rocks", rocksControllers);
 
 app.get("*", (req, res) => {
   res.status(404).json("Page not found");
